cded342d1d63ceaf8e6dc6b11944fd149c952175
Ablet Adem <ablet0991@hotmail.com>
2022-11-22 19:23:45 +0300

    assignment done

--- functions.js
+++ functions.js
@@ -1,10 +1,44 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (numara) => {
+  let odds = numara.filter((num) => num %2===1);
+  return odds.length;
+};
 
-const getIndexOfFalseItem = () => {};
+console.log(findTheNumberOfOddNumbers([3, 6, 7, 8, 4, 2]));
+console.log(findTheNumberOfOddNumbers([5, 91, 67, 45, 6, 22]));
 
-const filterNumbers = () => {};
+// //-------------- 2 -----------------------------
 
-const generateEmailAddress = () => {};
+let item = [false, 'Hi', true, 23, 'false', 55];
+const getIndexOfFalseItem = (pItem) => {
+  let indexNumber = pItem.indexOf(false)
+  return indexNumber;
+};
+console.log(getIndexOfFalseItem([45, 'Hi', false, true, 23, 'Coders']));
+console.log(getIndexOfFalseItem([false, 'Hi', true, 23, 'false', 55]));
+
+
+
+// //-------------- 3 ----------------------------
+
+
+const filterNumbers = (pArray) => {
+  let myNmr = pArray.filter(elmnt => typeof elmnt == "number")
+  console.log(myNmr)
+  return myNmr;
+};
+
+filterNumbers([false, 'Hi', true, 23, 'false', 55]);
+
+
+//-------------- 4 -----------------------------
+
+const generateEmailAddress = (pArray) => {
+  let newArray = pArray.map((ablet) => {
+    return ablet.toLowerCase() + "@gmail.com"
+  });
+  return newArray;
+};
+ generateEmailAddress(['Huseyin', 'Ahmet', 'Mehmet']);
 
 export {
   findTheNumberOfOddNumbers,
